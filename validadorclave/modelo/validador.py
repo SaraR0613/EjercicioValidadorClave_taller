07ad813bb24915b37ed3fc4f82faa292c747b7e7
B12_309_11\b12s309 <saritarojas0613@gmail.com>
2024-10-17 11:34:12 -0500

    validar_longitudl, contiene mayusculas, minusculas y numero y correcciones

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -1,14 +1,15 @@
 # TODO: Implementa el código del ejercicio aquí
 
 from abc import ABC, abstractmethod
-from validadorclave.modelo.errores import NoCumpleLongitudMinimaError
+from validadorclave.modelo.errores import NoCumpleLongitudMinimaError, NoTieneLetraMayusculaError, \
+    NoTieneLetraMinusculaError, NoTieneNumeroError
 
 
 class ReglaValidacion(ABC):
 
     @abstractmethod
 
-    def es_valida(self):
+    def es_valida(self, clave: str) -> bool:
         ...
 
     def __init__(self, longitud_esperada: int, ) :
@@ -21,39 +22,36 @@ class ReglaValidacion(ABC):
         if not self.longitud_esperada >= 8:
             raise NoCumpleLongitudMinimaError(" La clave debe tener una longitud de más de 8 caracteres")
 
+
     def _contiene_mayuscula(self, clave: str) -> bool:
-        self.letra_mayuscula: str
         self.clave: str = clave
 
-        return any(self.letra_mayuscula.isupper() for self.letra_mayuscula in self.clave)
+        if not clave.isupper():
+            raise NoTieneLetraMayusculaError
+
+        return True
 
 
     def _contiene_minuscula(self, clave: str) -> bool:
         self.clave: str = clave
         self.letra_miniscula: str
 
-        return any(self.letra_miniscula.islower() for self.letra_miniscula in self.clave)
+        if not clave.islower():
+            raise NoTieneLetraMinusculaError
+
+        return True
 
     def _contiene_numero(self, clave: str) -> bool:
         self. clave: str = clave
         self.numero: int
 
-        return any(self.numero.isdigit() for self.numero in self.clave)
+        if not clave.isdigit():
+            raise NoTieneNumeroError
+
+        return True
 
 
-class Validador(ReglaValidacion):
-
-    def __init__(self, regla: ReglaValidacion, longitud_esperada: int):
-        super().__init__(longitud_esperada)
-        self.regla: ReglaValidacion = regla
-
-    def es_valida(self, clave: str) -> bool:
-        ...
 
 
-class ReglaValidacionGanimedes (ReglaValidacion):
 
-    def contiene_caracter_especial(self):
-        pass
 
-class ReglaValidacionCalisto
